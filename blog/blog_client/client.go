ed67aba2f8a88ca9e03db3e0eaa29171b51b5503
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2022-01-19 23:43:47 -0500

    [NEW] Blog server, client and proto. CreateBlogService

---
+++ blog/blog_client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"grpc-go-example/blog/blogpb"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := blogpb.NewBlogServiceClient(cc)
+
+	// Create blog
+	fmt.Println("Creating the blog")
+	blog := blogpb.Blog{
+		AuthorId: "Stephane",
+		Title:    "My First Blog",
+		Content:  "Content of the first blog",
+	}
+	createBlogRes, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: &blog})
+	if err != nil {
+		log.Fatalf("Unexpected error: %v", err)
+	}
+	fmt.Printf("Blog has been created: %v", createBlogRes)
+}
