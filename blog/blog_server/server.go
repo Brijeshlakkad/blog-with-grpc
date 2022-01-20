bbcebf39b91c35036860b72774b08ac0b8ef03c6
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2022-01-19 23:46:04 -0500

    evans reflection

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -17,6 +17,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 )
 
@@ -243,6 +244,8 @@ func main() {
 	opts := []grpc.ServerOption{}
 	s := grpc.NewServer(opts...)
 	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	reflection.Register(s)
 
 	go func() {
 		fmt.Println("Starting Server...")
